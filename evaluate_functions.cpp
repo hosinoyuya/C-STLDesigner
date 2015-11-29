b012bae3d14e3d5107040bab8229b96a1f33e9de
大平哲也 <odaira@islab.cs.tsukuba.ac.jp>
2015-11-29 21:20:02 +0900

    get score

--- evaluate_functions.cpp
+++ evaluate_functions.cpp
@@ -12,7 +12,14 @@ evaluate_functions::~evaluate_functions()
 }
 
 
-double evaluate_functions::calc_integral(vector<string> time_interval, vector<string> voltage1, vector<string> voltage2)
+double evaluate_functions::calc_integral(double time_interval, vector<double> &voltage1, vector<double> &voltage2)
 {
-	return 1.0;
+	double diff1, diff2;
+	double score = 0;
+	for (size_t i = 0; i < voltage1.size()-1; i++) {
+		diff1 = fabs(voltage2[i] - voltage1[i]);
+		diff2 = fabs(voltage2[i+1] - voltage1[i+1]);
+		score += (diff1 + diff2) * time_interval / 2.0;
+	}
+	return score;
 }
--- evaluate_functions.h
+++ evaluate_functions.h
@@ -9,6 +9,6 @@ class evaluate_functions
 public:
 	evaluate_functions();
 	~evaluate_functions();
-	static double calc_integral(vector<string> time_interval, vector<string> voltage1, vector<string> voltage2);
+	static double calc_integral(double time_interval, vector<double> &voltage1, vector<double> &voltage2);
 };
 
